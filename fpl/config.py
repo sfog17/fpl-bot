233473a8266931241dfd41a9e53795e170e9cc4e
sfog17 <>
2018-12-01 16:55:31 +0000

    download_data

---
+++ fpl/config.py
@@ -0,0 +1,20 @@
+import pathlib
+
+# Structure Data
+DIR_DATA = pathlib.Path(__file__).parent.parent.joinpath('data')
+
+DIR_RAW_HIST = DIR_DATA.joinpath('raw', 'historical')
+DIR_DATA_RAW_BOOTSTRAP = DIR_DATA.joinpath('raw', 'bootstrap')
+DIR_RANKINGS = DIR_DATA.joinpath('raw', 'rankings')
+
+FILE_INTER_HISTORICAL = DIR_DATA.joinpath('intermediate', 'historical.csv')
+FILE_INTER_BOOTSTRAP = DIR_DATA.joinpath('intermediate', 'bootstrap.csv')
+FILE_INTER_BOOTSTRAP_FEAT = DIR_DATA.joinpath('intermediate', 'bootstrap_features.csv')
+FILE_INTER_FEATURES = DIR_DATA.joinpath('intermediate', 'features.csv')
+
+DIR_DATA_RESULTS = DIR_DATA.joinpath('results')
+
+# URL Scrape
+URL_BASE = 'https://fantasy.premierleague.com/drf/'
+URL_BOOTSTRAP = URL_BASE + 'bootstrap-static'
+URL_PLAYERS = URL_BASE + 'element-summary'
---
+++ fpl/main.py
@@ -0,0 +1,5 @@
+from fpl.utils import download
+
+if __name__ == '__main__':
+    # download.get_historical('fpl_api_historical')
+    # download.get_rankings('rankings_test', 10)
