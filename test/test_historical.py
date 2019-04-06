db4bc7e3c65f6e788f37b5951f69e590fa658e90
sfog17 <>
2019-04-06 14:48:17 +0100

    test - correct behaviour (outer join, not left)

--- test/test_historical.py
+++ test/test_historical.py
@@ -7,13 +7,19 @@ pd.options.display.max_columns = 5
 
 
 def test_simple():
-    df_simple = pd.DataFrame({
+    df_input = pd.DataFrame({
         fld.PLAYER_ID: [100, 100, 100],
         fld.SEASON_ID: [9, 10, 11],
         TOTAL_POINTS: [20, 50, 100]
     })
-    df_avg = pd.DataFrame({fld.STAT_POINTS_AVG_SEASON_PREV: [np.nan, 0.53, 1.32]})
-    df_result = add_avg_prev_season(df_simple)
-    df_expected = pd.concat([df_simple, df_avg], axis=1)
+
+    df_expected = pd.DataFrame({
+        fld.PLAYER_ID: [100, 100, 100, 100],
+        fld.SEASON_ID: [9, 10, 11, 12],
+        TOTAL_POINTS: [20, 50, 100, np.nan],
+        fld.STAT_POINTS_AVG_SEASON_PREV: [np.nan, 0.53, 1.32, 2.63]
+    })
+
+    df_result = add_avg_prev_season(df_input)
 
     assert_frame_equal(df_result, df_expected)
