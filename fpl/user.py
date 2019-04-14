2b9eaa732ab3b8d3c3b5c32331546cb45ef2dc8d
sfog17 <>
2019-04-14 22:04:07 +0100

    user - break down in two modules (for optimise)

--- fpl/user.py
+++ fpl/user.py
@@ -1,38 +1,22 @@
 import logging
 import pandas as pd
 import requests
-from dataclasses import dataclass
-from enum import Enum
 from typing import List
 from fpl.collect.download import get_user_info
+from user_player import UserPlayer, PlayerRole
 
 logger = logging.getLogger(__name__)
 
 
-class PlayerRole(Enum):
-    """ Role of the football player in the FPL gameweek """
-    BENCH = 0
-    NORMAL = 1
-    CAPTAIN = 2
-    VICE_CAPTAIN = 3
-
-@dataclass
-class UserPlayer(object):
-    """ Football Player that can be selected by the user """
-    id: int
-    role: PlayerRole
-    purchase_price: float
-    selling_price: float
-
 class UserTeam(object):
     """ Team of the user in the FPL website """
-    money_bank: int
+    money_bank: int 
     money_total: int
     free_transfers: int
     players: List[UserPlayer]
     unlimited_transfers: bool
 
-    def __init__(self, money: int = 100, unlimited_transfers: bool = False):
+    def __init__(self, money: int = 1000, unlimited_transfers: bool = False):
         self.money_bank = money
         self.money_total = money
         self.free_transfers = 15
@@ -49,7 +33,7 @@ class UserTeam(object):
         # Get players
         self.players = []
         for pick in user_info['picks']:
-            current_player = UserPlayer(id=pick['element'], 
+            current_player = UserPlayer(id_season=pick['element'], 
                                         role=self._get_role(pick),
                                         purchase_price=pick['purchase_price'], 
                                         selling_price=pick['selling_price'])
---
+++ fpl/user_player.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from enum import Enum
+
+class PlayerRole(Enum):
+    """ Role of the football player in the FPL gameweek """
+    BENCH = 0
+    NORMAL = 1
+    CAPTAIN = 2
+    VICE_CAPTAIN = 3
+
+@dataclass
+class UserPlayer(object):
+    """ Football Player that can be selected by the user """
+    id_season: int
+    role: PlayerRole
+    purchase_price: float
+    selling_price: float
